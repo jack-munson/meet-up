194559e6451ef4c69829db957993a49e45feeccb
Jack Munson <jack.munson@duke.edu>
2024-06-04 00:41:17 -0400

    Added Google sign in and sign up redirecting

--- src/routes/Signin.jsx
+++ src/routes/Signin.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { useNavigate } from 'react-router-dom'
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, getAdditionalUserInfo } from 'firebase/auth'
 import MainLogo from "../public/MeetUp-main-logo.png"
 import "../styles/Authentication.css"
 
@@ -24,6 +24,25 @@ export function Signin() {
         })
     }
 
+    async function handleGoogle(e) {
+        e.preventDefault()
+        const provider = new GoogleAuthProvider()
+        signInWithPopup(auth, provider)
+        .then(function (result) {
+            const user = result.user
+            const additionUserInfo = getAdditionalUserInfo(result)
+            if (additionUserInfo.isNewUser) {
+                navigate('/signup')
+            }
+            else {
+                navigate('/home')
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+
     return (
         <div className="auth-box">
             <form action="">
@@ -32,7 +51,7 @@ export function Signin() {
                 <input onChange={(e) => {setPassword(e.target.value)}} type="text" placeholder="Password"/>
                 <button className="signin-button" type="button" onClick={(e) => {handleSignIn(e)}}>Sign In</button>
                 <span className="instruction-small">or continue with</span>
-                <button className="signin-google-button">Google</button>
+                <button className="signin-google-button" onClick={(e) => {handleGoogle(e)}}>Google</button>
                 <span className="instruction-medium">Don't have an account?</span>
                 <button className="signup-button">Sign up</button>
             </form>
