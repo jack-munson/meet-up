b4f9d5173e4d5c15804c6e74a4e1727ca649f5e6
Jack Munson <jack.munson@duke.edu>
2024-06-04 23:56:55 -0400

    Completed initial welcome/front page

--- src/routes/Front.jsx
+++ src/routes/Front.jsx
@@ -9,7 +9,35 @@ export function Front() {
             <FrontHeader></FrontHeader>
             <div className="page-content">
                 <div className="welcome-message">
-                    Welcome
+                    <div className="welcome-text-main">Welcome to</div>
+                    <div className="welcome-text-main" style={{ color: "#0040B4" }}>MeetUp!</div>
+                    <div className="welcome-text-sub">We make group scheduling easy</div>
+                    <div className="welcome-message-text-steps">
+                        <div className="message-step">
+                            <div className="step-number">1.</div>
+                            <div className="step-messages">
+                                <div className="step-message">Create a meeting and invite</div>
+                                <div className="step-message">the attendees.</div>
+                            </div>
+                        </div>
+                        <div className="message-step">
+                            <div className="step-number">2.</div>
+                            <div className="step-messages">
+                                <div className="step-message">Once everyone has input their </div>
+                                <div className="step-message">availability, let our algorithm </div>
+                                <div className="step-message">find the best time for your group </div>
+                                <div className="step-message">to meet.</div>
+                            </div>
+                        </div>
+                        <div className="message-step">
+                            <div className="step-number">3.</div>
+                            <div className="step-messages">
+                                <div className="step-message">Choose a time and generate </div>
+                                <div className="step-message">a Zoom or Google Meet link </div>
+                                <div className="step-message">right from MeetUp.</div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <CreateMeeting></CreateMeeting>
             </div>
