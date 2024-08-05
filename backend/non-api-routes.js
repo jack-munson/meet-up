03bf24d4cf1cd5587cf5dca2658272de6f10eed2
jack-munson <jack.munson21@gmail.com>
2024-08-04 19:16:38 -0600

    Working on Zoom bug

--- backend/non-api-routes.js
+++ backend/non-api-routes.js
@@ -8,8 +8,8 @@ router.get('/', (req, res) => {
 
 router.get('/callback', async (req, res) => {
     console.log("Made it to /callback")
-    const authorizationCode = req.query.code;
-    const originalUrl = req.query.state;
+    const authorizationCode = req.query.code
+    const originalUrl = req.query.state
     
     try {
         const response = await axios.post('https://zoom.us/oauth/token', null, {
@@ -21,24 +21,24 @@ router.get('/callback', async (req, res) => {
             headers: {
                 Authorization: `Basic ${Buffer.from(`${process.env.ZOOM_CLIENT_ID}:${process.env.ZOOM_CLIENT_SECRET}`).toString('base64')}`
             }
-        });
+        })
 
-        const accessToken = response.data.access_token;
-        console.log("Access Token:", accessToken);
+        const accessToken = response.data.access_token
+        console.log("Access Token:", accessToken)
 
         const script = `
             <script>
                 window.opener.postMessage({ zoomAccessToken: '${accessToken}' }, '${originalUrl}');
                 window.close();
             </script>
-        `;
+        `
         
-        res.send(script);
+        res.send(script)
     } catch (error) {
-        console.log('Error exchanging authorization code for access token:', error);
-        res.status(500).send('Internal server error ' + error + '!');
+        console.log('Error exchanging authorization code for access token:', error)
+        res.status(500).send('Internal server error: ' + error + '| Authorization code: ' + authorizationCode + '| Redirect URI: ' + redirect_uri)
     }
 });
 
 
-module.exports = router;
+module.exports = router
